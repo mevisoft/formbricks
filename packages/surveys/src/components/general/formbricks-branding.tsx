44b5b9e5c312c93624d676928c7a248ceeddaef3
Luis Mejia <iluismejialeon@gmail.com>
2025-12-20 18:42:46 -0500

    Remove `--verbose` flag in Dockerfile and comment out unused Formbricks branding translation code

--- packages/surveys/src/components/general/formbricks-branding.tsx
+++ packages/surveys/src/components/general/formbricks-branding.tsx
@@ -1,7 +1,7 @@
-import { useTranslation } from "react-i18next";
+// import { useTranslation } from "react-i18next";
 
 export function FormbricksBranding() {
-  const { t } = useTranslation();
+  // const { t } = useTranslation();
   return (
     <span className="flex justify-center">
      {/* <a
