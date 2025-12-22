b46a476f2a762a8cf10d247c96ccfcecc3a99895
Luis Mejia <iluismejialeon@gmail.com>
2025-12-22 18:56:44 -0500

    Add missing `data-ml` and correct `dataTitle` to `data-title` attributes

--- packages/surveys/src/components/general/formbricks-branding.tsx
+++ packages/surveys/src/components/general/formbricks-branding.tsx
@@ -3,6 +3,6 @@ import { useTranslation } from "react-i18next";
 export function FormbricksBranding() {
   const { t } = useTranslation();
   return (
-    <span className="flex justify-center" dataTitle={t("common.powered_by")} />
+    <span className="flex justify-center" data-title={t("common.powered_by")} />
   );
 }
