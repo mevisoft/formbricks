f1147d2915f4695f9659e192e0f036e768f37f97
Luis Mejia <iluismejialeon@gmail.com>
2025-12-23 11:50:13 -0500

    Fix: Remove unused import and update  attribute in MainNavigation component

--- apps/web/app/(app)/environments/[environmentId]/components/MainNavigation.tsx
+++ apps/web/app/(app)/environments/[environmentId]/components/MainNavigation.tsx
@@ -16,7 +16,6 @@ import Link from "next/link";
 import { usePathname, useRouter } from "next/navigation";
 import { useEffect, useMemo, useState } from "react";
 import { useTranslation } from "react-i18next";
-import { undefined } from "zod";
 import { TEnvironment } from "@formbricks/types/environment";
 import { TOrganizationRole } from "@formbricks/types/memberships";
 import { TOrganization } from "@formbricks/types/organizations";
@@ -126,7 +125,7 @@ export const MainNavigation = ({
       label: t("common.account"),
       href: `/environments/${environment.id}/settings/profile`,
       icon: UserCircleIcon,
-      target: "self",
+      target: "_self",
     },
   ];
 
