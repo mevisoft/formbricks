48967de47a05675c4a593d8461109f8f65b0b901
Mevisoft <56488338+mevisoft@users.noreply.github.com>
2025-12-26 15:03:24 -0500

    Include instanceId in telemetry resolve payload

--- apps/web/app/api/(internal)/pipeline/lib/telemetry.ts
+++ apps/web/app/api/(internal)/pipeline/lib/telemetry.ts
@@ -267,7 +267,7 @@ const sendTelemetry = async (lastSent: number) => {
         body: JSON.stringify(payload),
         signal: controller.signal,
       }); */
-      resolve(payload);
+      resolve({payload, instanceId});
     } catch (e) {
       resolve(0);
     }
